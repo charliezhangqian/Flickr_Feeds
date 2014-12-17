8da661644b79877750dc7885563a18eedd8d0bf2
charliezhangqian <charlie_zq@hotmail.com>
2014-12-17 15:46:34 +1100

    Update views.py
    change the url and function name
    add comments

--- app/views.py
+++ app/views.py
@@ -1,23 +1,23 @@
 from flask import render_template, flash, redirect
 from app import app
-from forms import LoginForm
+from forms import SearchForm
 import httplib
 import re
 
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-    form = LoginForm()
+@app.route('/flickr', methods = ['GET', 'POST'])
+def search():
+    form = SearchForm()
     m = []
     if form.validate_on_submit():
     	keywords = form.query.data
     	p = re.compile("\s")
-    	keywords = p.sub("",keywords)
+    	keywords = p.sub("",keywords) #remove the spaces in keywords
     	conn = httplib.HTTPConnection("www.flickr.com")
     	url = "http://api.flickr.com/services/feeds/photos_public.gne?tags="+keywords;
-    	conn.request("GET", url)
+    	conn.request("GET", url)  #send Http GET request to flickr web feeds
     	response = conn.getresponse()
         data = response.read();
-        p = re.compile("img src.*?(http.*?\.jpg).*?enclosure.*?(http.*?\.jpg)", re.S)
+        p = re.compile("img src.*?(http.*?\.jpg).*?enclosure.*?(http.*?\.jpg)", re.S)  #parse the HTTP response
         m = p.findall(data)
     return render_template('search.html',
         title = 'Flickr Search',
