87dfaf03c38100e27d1f20c9fae5b90c74e5b0c2
charliezhangqian <charlie_zq@hotmail.com>
2015-01-04 22:07:39 +1100

    update to stage 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
- 
+from flaskext.mysql import MySQL
 app = Flask(__name__)
 app.config.from_object('config')
+
+mysql = MySQL()
+# app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'pictures'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
  
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,15 +1,53 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from app import app
 from forms import SearchForm
 import httplib
 import re
+from app import mysql
+
+#used for adding img url into database
+def LikeImage(smallURL, bigURL):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * from liked where url_s = '"+smallURL+"'")
+    data = cursor.fetchone()
+    if data is None:
+        cursor.execute("INSERT into liked values ('', '"+smallURL+"','"+bigURL+"')")
+        conn.commit()
+        cursor.close()
+        print "INSERT"
+        return "This image is liked"
+    else:
+        cursor.close()
+        print "already liked"
+        return "This image has already been liked"
+
+#used for querying liked images from database
+def reviewLiked():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * from liked")
+    data = cursor.fetchall()
+    cursor.close()
+    print "view"
+    return [dict(small=row[1], big=row[2]) for row in data]
+
+#used for removing disliked image
+def dislikeImage(smallURL, bigURL):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("DELETE from liked where url_s = '"+smallURL+"'")
+    conn.commit()
+    print "delete"
 
 @app.route('/flickr', methods = ['GET', 'POST'])
 def search():
-    form = SearchForm()
+    form1 = SearchForm()
     m = []
-    if form.validate_on_submit():
-    	keywords = form.query.data
+    entries = {}
+    message = None
+    if form1.validate_on_submit():
+    	keywords = form1.query.data
     	p = re.compile("\s")
     	keywords = p.sub("",keywords) #remove the spaces in keywords
     	conn = httplib.HTTPConnection("www.flickr.com")
@@ -19,7 +57,24 @@ def search():
         data = response.read();
         p = re.compile("img src.*?(http.*?\.jpg).*?enclosure.*?(http.*?\.jpg)", re.S)  #parse the HTTP response
         m = p.findall(data)
+
+    elif request.form.get("like") != None:
+        smallURL = request.form.get("small")
+        bigURL = request.form.get("big")
+        message = LikeImage(smallURL, bigURL)
+
+    elif request.form.get("review") != None:
+        entries = reviewLiked()
+        
+    elif request.form.get("dislike") != None:
+        smallURL = request.form.get("small")
+        bigURL = request.form.get("big")
+        dislikeImage(smallURL, bigURL)
+        entries = reviewLiked()
     return render_template('search.html',
         title = 'Flickr Search',
-        form = form,
-        imgs = m)
+        form1 = form1,
+        liked = entries,
+        imgs = m,
+        message = message)
+    
